497aaa2089a022ae4d9e009b3dbd7b856b622a51
lchester13 <leanna.r.chester@gmail.com>
2023-07-18 15:08:42 -0700

    Initial Commit

---
+++ uber-clone/uber_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  uber_cloneApp.swift
+//  uber-clone
+//
+//  Created by Leanna Chester on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct uber_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
