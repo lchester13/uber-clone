63ad7d41ac993a326edae7d4f91911542aaa3b19
lchester13 <leanna.r.chester@gmail.com>
2023-07-26 14:45:28 -0700

    adding animations, buttons, list UI

--- uber-clone/Core/HomeView.swift
+++ uber-clone/Core/HomeView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var showLocationSearchView = false
     var body: some View {
         ZStack(alignment:.top) {
             UberMapViewRepresentable()
                 .ignoresSafeArea()
+            if !showLocationSearchView {
+                LocationSearchView()
+            } else {
+                LocationSearchActivationView()
+                    .padding(.top, 72)
+                    .onTapGesture {
+                        withAnimation(.spring()) {
+                            showLocationSearchView.toggle()
+                        }
+
+                    }
+            }
             
-            LocationSearchActivationView()
-                .padding(.top, 72)
-            MapViewActionButton()
+            
+            MapViewActionButton(showLocationSearchView: $showLocationSearchView)
                 .padding(.leading)
                 .padding(.top, 4)
         }
